6f9eb93ff59696e277116427b1ba58743bd729d4
Matheus <furlan.dmtr@gmail.com>
2021-03-12 07:17:40 -0400

    Correção do EsModule

--- 04javaScript/atividade/script.js
+++ 04javaScript/atividade/script.js
@@ -2,3 +2,11 @@ export  var arr = ['Olá! :D', 'Sério', 'Tudo!', 'Sei não em', 'Peixe'];
 export function alertBox(ele) {
     window.alert(ele)
 };
+
+export function setup(){
+    document.querySelectorAll("button").forEach((button,i) => {
+        button.addEventListener("click", function(){
+            alertBox(arr[i]);
+        });
+    });
+}
